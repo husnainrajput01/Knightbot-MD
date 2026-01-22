32acefffdf355a209b60fe9db8a3d95bbddcee40
Husnain Rajput <141842546+husnainrajput01@users.noreply.github.com>
2026-01-22 09:06:35 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "𝕯𝖆𝖓𝖌𝖊𝖗𝕏ꜰᴇᴀʀ-MD",
+  botOwner: '𝕯𝕏ꜰ(HUSNAIN🇵🇰)', // Your name
+  ownerNumber: '923010609759', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.6",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/husnainrajput01/Knightbot-MD/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
