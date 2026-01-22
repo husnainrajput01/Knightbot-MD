1703f3a67785d5ee4f54f112b636d9c2d96e5444
Husnain Rajput <141842546+husnainrajput01@users.noreply.github.com>
2026-01-22 09:45:37 +0500

    Update autostatus.js

--- commands/autostatus.js
+++ commands/autostatus.js
@@ -7,8 +7,8 @@ const channelInfo = {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterJid: '120363420537894461@newsletter',
+            newsletterName: '𝕯𝕏ꜰ(HUSNAIN🇵🇰)',
             serverMessageId: -1
         }
     }
