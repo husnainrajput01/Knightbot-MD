7ab471ddd46e0eab167f991579c5effddf3667a4
Husnain Rajput <141842546+husnainrajput01@users.noreply.github.com>
2026-01-22 09:44:16 +0500

    Update autoread.js

--- commands/autoread.js
+++ commands/autoread.js
@@ -31,8 +31,8 @@ async function autoreadCommand(sock, chatId, message) {
                     forwardingScore: 1,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363161513685998@newsletter',
-                        newsletterName: 'KnightBot MD',
+                        newsletterJid: '120363420537894461@newsletter',
+                        newsletterName: '𝕯𝕏ꜰ(HUSNAIN🇵🇰)',
                         serverMessageId: -1
                     }
                 }
@@ -62,8 +62,8 @@ async function autoreadCommand(sock, chatId, message) {
                         forwardingScore: 1,
                         isForwarded: true,
                         forwardedNewsletterMessageInfo: {
-                            newsletterJid: '120363161513685998@newsletter',
-                            newsletterName: 'KnightBot MD',
+                            newsletterJid: '120363420537894461@newsletter',
+                            newsletterName: '𝕯𝕏ꜰ(HUSNAIN🇵🇰)',
                             serverMessageId: -1
                         }
                     }
@@ -85,8 +85,8 @@ async function autoreadCommand(sock, chatId, message) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '120363420537894461@newsletter',
+                    newsletterName: '𝕯𝕏ꜰ(HUSNAIN🇵🇰)',
                     serverMessageId: -1
                 }
             }
@@ -100,8 +100,8 @@ async function autoreadCommand(sock, chatId, message) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '120363420537894461@newsletter',
+                    newsletterName: '𝕯𝕏ꜰ(HUSNAIN🇵🇰)',
                     serverMessageId: -1
                 }
             }
